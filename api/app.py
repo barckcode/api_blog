1d985c1803b3531a4fdc7abd8757293d5aeea545
BarckCode <barckcode@gmail.com>
2021-07-29 19:43:35 +0200

    Added more description to my API

--- api/app.py
+++ api/app.py
@@ -3,7 +3,15 @@ from routes.blogpost import blog_post
 
 
 # Init FastAPI
-app = FastAPI()
+app = FastAPI(
+    title = "Blog API",
+    description = "Endpoints para administrar todo lo relacionado con el Blog",
+    version = 0.1,
+    openapi_tags = [{
+        "name": "Posts",
+        "description": "Endpoint of Posts"
+    }]
+)
 
 
 # Include Routes to FastAPI
--- api/routes/blogpost.py
+++ api/routes/blogpost.py
@@ -5,21 +5,22 @@ from config.db import db_connection
 from models.posts import all_posts_table
 from schemas.post import Post_Model
 
+
 # Init Routes
 blog_post = APIRouter()
 
 # Routes
-@blog_post.get("/posts")
+@blog_post.get("/posts", response_model=Post_Model)
 def get_all_posts():
     return db_connection.execute(all_posts_table.select()).fetchall()
 
 
-@blog_post.get("/posts/{id}")
+@blog_post.get("/posts/{id}", response_model=Post_Model)
 def get_post_by_id(id: int):
     return db_connection.execute(all_posts_table.select().where(all_posts_table.c.id == id)).first()
 
 
-@blog_post.post("/posts")
+@blog_post.post("/posts", response_model=Post_Model)
 def create_new_post(post: Post_Model):
     new_post = {
         "category": post.category,
@@ -33,13 +34,13 @@ def create_new_post(post: Post_Model):
     return db_connection.execute(all_posts_table.select().where(all_posts_table.c.id == result.lastrowid)).first()
 
 
-@blog_post.delete("/posts/{id}")
+@blog_post.delete("/posts/{id}", status_code=HTTP_204_NO_CONTENT)
 def delete_post_by_id(id: int):
     db_connection.execute(all_posts_table.delete().where(all_posts_table.c.id == id))
     return Response(status_code=HTTP_204_NO_CONTENT)
 
 
-@blog_post.put("/posts/{id}")
+@blog_post.put("/posts/{id}", response_model=Post_Model)
 def update_post_by_id(id: int, post: Post_Model):
     db_connection.execute(all_posts_table.update().values(
         category = post.category,
